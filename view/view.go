16f83c6716075e68ed199123c565d84f87727d0d
Ray <raygenbogen@tinymantico.re>
2014-06-11 13:21:07 +0200

    fixed playback of videofiles, where there are spaces in the path

--- view/view.go
+++ view/view.go
@@ -416,7 +416,9 @@ func FileHandler (w http.ResponseWriter, r *http.Request, path string){
    		title := "Videoplayer"
    		replace := strings.NewReplacer("webm", "vtt")
    		subpath := replace.Replace(newpath)
-   		HTMLAttr :="<tr><td><video width=\"100%\" height=\"80%\" preload=\"auto\" controls><source src="+newpath+" type=video/webm /><track src="+subpath+" kind=\"subtitle\" src=\"de-DE\" label=\"german\"/>Your browser does not support the video tag.</video></td></tr>"
+   		println(path)
+   		println(newpath)
+   		HTMLAttr :="<tr><td><video width=\"100%\" height=\"80%\" preload=\"auto\" controls><source src=\""+newpath+"\" type=video/webm /><track src="+subpath+" kind=\"subtitle\" src=\"de-DE\" label=\"german\"/>Your browser does not support the video tag.</video></td></tr>"
 		println(subpath)
 		dBody = template.HTML(HTMLAttr)
 		renderTemplate(w, "files", &Page{Title: title , DisplayBody: dBody})
