5bf086bab531f4762f287b4b12e1f0feedbba7ca
eric <mam09bog@studserv.uni-leipzig.de>
2015-03-04 22:24:05 +0100

    reading login information per database

--- auth/auth.go
+++ auth/auth.go
@@ -10,6 +10,9 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
 )
 
 var templates = template.Must(template.ParseFiles("./static/templates/main.html", "./static/templates/head.html", "./static/templates/menu.html", "./static/templates/content_auth.html", "./static/templates/footer.html"))
@@ -30,53 +33,75 @@ type User struct {
 	Adminstatus    string
 }
 
+func dbOpen () *sql.DB {
+	db, err := sql.Open("postgres", "user=postgres dbname=wiki sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+		}
+	return db
+}
+
 func Auth(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		renderAuth(w, "")
 	case "POST":
+
 		username := r.FormValue("username")
 		enteredPassword := r.FormValue("password")
-		filename := "./users/" + username
-		if _, err := os.Stat(filename); os.IsNotExist(err) {
+
+		db := dbOpen()
+
+		rows, err := db.Query("SELECT password, approved, admin FROM users WHERE name = $1", username)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var password, approved, admin string
+		var user User
+
+		count_rows := 0
+		for rows.Next() {
+			count_rows += 1
+			err := rows.Scan(&password, &approved, &admin)
+			if err != nil {
+					log.Fatal(err)
+			}
+			user = User{username, password, approved, admin}
+		}
+		if count_rows == 0 {
 			renderAuth(w, "This User doesn't seem to exist!")
 			return
+		}
+		defer rows.Close()
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		} else {
-			file, erro := os.Open(filename)
-			if erro != nil {
-				println("Error opening the file")
-			}
-			defer file.Close()
-			decoder := json.NewDecoder(file)
-			var user User
-			err := decoder.Decode(&user)
+		if user.Approvalstatus == "approved" {
+			err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(enteredPassword))
 			if err != nil {
-				println("Error decoding json")
-			}
-			if user.Approvalstatus == "approved" {
-				err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(enteredPassword))
-				if err != nil {
-					renderAuth(w, "Wrong Password!")
-
-				} else {
-					t := time.Now()
-					expiration := time.Now().AddDate(1, 0, 0)
-					h := md5.New()
-					const layout = "2006-01-02 15:04:05"
-					hashedTime := t.Format(layout)
-					hashedStoredPassword := hex.EncodeToString(h.Sum([]byte(user.Password)))
-					cookie := http.Cookie{Name: "User", Value: username, Path: "/", Expires: expiration}
-					cookie2 := http.Cookie{Name: hashedStoredPassword, Value: hashedTime, Path: "/", Expires: expiration}
-					http.SetCookie(w, &cookie)
-					http.SetCookie(w, &cookie2)
-					http.Redirect(w, r, "/view/start", http.StatusFound)
-
-				}
+				renderAuth(w, "Wrong Password!")
 
 			} else {
-				renderAuth(w, "You are not approved. Please contact the Administrator!")
+				t := time.Now()
+				expiration := time.Now().AddDate(1, 0, 0)
+				h := md5.New()
+				const layout = "2006-01-02 15:04:05"
+				hashedTime := t.Format(layout)
+				hashedStoredPassword := hex.EncodeToString(h.Sum([]byte(user.Password)))
+				cookie := http.Cookie{Name: "User", Value: username, Path: "/", Expires: expiration}
+				cookie2 := http.Cookie{Name: hashedStoredPassword, Value: hashedTime, Path: "/", Expires: expiration}
+				http.SetCookie(w, &cookie)
+				http.SetCookie(w, &cookie2)
+				http.Redirect(w, r, "/view/start", http.StatusFound)
+
 			}
+
+		} else {
+			renderAuth(w, "You are not approved. Please contact the Administrator!")
+
 		}
 	}
 }
